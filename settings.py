84f3d00fac80b7dc1d2f7864b640238cb8523842
letsdie10 <sngby98@gmail.com>
2017-09-30 05:30:31 +0100

    Added comments

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
 class Settings():
 	def __init__(self):
+		"""Initialize all the static settings of the game."""
+		# Screen settings.
 		self.screen_width = 600
 		self.screen_height = 600
 		self.bg_color = (192, 192, 192)
 		
+		# Pacman settings.
 		self.pacman_speed_factor = 0.3
-		
+		# Pacman move settings.
 		self.pacman_time_move = 0
 		self.pacman_time_move_interval = 0.2
 		
