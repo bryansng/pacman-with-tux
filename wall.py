a4dc2bd3c5a7c5a930439afa6ecf7ba279f50c7d
letsdie10 <sngby98@gmail.com>
2017-09-30 05:30:37 +0100

    Added comments

--- wall.py
+++ wall.py
@@ -2,17 +2,22 @@ import pygame
 from pygame.sprite import Sprite
 
 class Wall(Sprite):
+	"""A class to represent the balls eaten by Pacman."""
 	def __init__(self, ai_settings, screen):
+		"""Initializes Wall settings."""
 		super().__init__()
 		self.ai_settings = ai_settings
 		self.screen = screen
 		
+		# Loads image and get image rect.
 		self.image = pygame.image.load('images/wall.bmp')
 		self.rect = self.image.get_rect()
 		self.screen_rect = self.screen.get_rect()
 		
+		# Position the wall/image to specified spot.
 		self.rect.x, self.rect.y = 0, 0
 		
+		# Float values to a new variable if not decimals will not be registered.
 		self.centerx = float(self.rect.centerx)
 		self.centery = float(self.rect.centery)
 		
@@ -20,6 +25,8 @@ class Wall(Sprite):
 		#self.y = float(self.rect.y)
 		
 	def update(self):
+		"""Updates the position of the Wall."""
+		# Float values are converted to integers and assigned back to the rect.
 		self.rect.centerx = self.centerx
 		self.rect.centery = self.centery
 		
@@ -27,4 +34,7 @@ class Wall(Sprite):
 		#self.rect.y = self.y
 		
 	def blitme(self):
+		"""Draws the Wall onto the screen."""
+		# Only for individual walls, sprites are prefered to use 
+		# self.draw(self.screen) or walls.draw(screen)
 		self.screen.blit(self.image, self.rect)
